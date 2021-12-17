76a8bb985370e4a7d35fe9a626ac2025b13f9402
Dave Quinn <dquinn8@cogeco.ca>
2021-12-16 19:49:02 -0500

    completed thoughts model with reactions schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,33 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema({
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: `You need to provide a Reaction body!`,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+}, {
+    toJSON: {
+        getters: true
+    }
+});
+
 const ThoughtSchema = new Schema({
     thoughtText: {
         type: String,
@@ -14,24 +41,22 @@ const ThoughtSchema = new Schema({
         default: Date.now,
         get: (createdAtVal) => dateFormat(createdAtVal)
     },
-    thoughts: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Thought'
-    }],
-    friends: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Thought'
-    }]
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
 }, {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 });
 
 // get total count of comments and replies on retrieval
-ThoughtSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.length;
 });
 
 // create the Thought model using the ThoughtSchema
